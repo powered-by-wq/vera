91258aa04eb115badf650e66c2975a05152e2a2a
S. Andrew Sheppard <andrew@wq.io>
2015-09-09 14:47:19 -0500

    use default python for tests

--- runtests.sh
+++ runtests.sh
@@ -3,6 +3,6 @@ if [ "$LINT" ]; then
     flake8 vera tests --exclude migrations
     flake8 vera/migrations --ignore E501
 else
-    python3 setup.py test
-    DJANGO_SETTINGS_MODULE=tests.swap_settings python3 setup.py test
+    python setup.py test
+    DJANGO_SETTINGS_MODULE=tests.swap_settings python setup.py test
 fi
