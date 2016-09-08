9c63c26474621b7efba97d631795cd0be5ba3e93
S. Andrew Sheppard <andrew@wq.io>
2016-09-07 21:33:43 -0500

    update versions

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Framework :: Django',
         'Topic :: Scientific/Engineering :: GIS',
         'Topic :: Database :: Database Engines/Servers',
