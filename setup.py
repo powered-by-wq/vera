bd6a7aed8bdafc8a3025aefa5d54616473f203a6
S. Andrew Sheppard <andrew@wq.io>
2017-09-14 10:44:18 -0500

    update version for dev

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def parse_markdown_readme():
 
 setup(
     name='vera',
-    version='1.0.0',
+    version='1.0.1-dev',
     author='S. Andrew Sheppard',
     author_email='andrew@wq.io',
     url='https://wq.io/vera',
